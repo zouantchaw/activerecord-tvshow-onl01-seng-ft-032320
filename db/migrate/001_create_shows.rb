55139a0985db0e77669192c2171fce14345842cf
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-27 12:46:03 -0400

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,11 @@
+#migration code to create shows table. 
+class CreateShows < ActiveRecord::Migration[5.2]
+    def change
+        create_table :shows do |t|
+            t.string :name 
+            t.string :network
+            t.string :day 
+            t.integer :rating 
+        end
+    end
+end 
